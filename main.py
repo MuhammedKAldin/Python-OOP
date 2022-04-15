648f28d5cd1e4b8813d8f36c506053ec9d9fdacc
MuhammedKAldin <54613628+MuhammedKAldin@users.noreply.github.com>
2022-04-15 16:34:04 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+# class Car:
+#     def __init__(self,brand,model,color,wheels):
+#         self.brand = brand
+#         self.model = model
+#         self.color = color
+#         self.wheels = wheels
+
+
+# cheverollet = Car("Chevrollete","Tahoe","red",5)
+# print(cheverollet.model)
+
+#====================================
+# Class Inhertiance
+# class Animal:
+#     def __init__(self,name):
+#         self.name = name
+        
+#     def eyeSight(self):
+#         print("I Can See")
+
+
+# class Cat(Animal):
+#     def purr(self):
+#         print("Meooow !")
+        
+# class Dog(Animal):
+#     def bark(self):
+#         print("Woof!")
+#         super().eyeSight() # => getting the Super class methods 
+        
+# germanShephard = Dog("Sam")
+# #germanShephard.eyeSight() # =>Already called inside bark() by the super() method 
+# germanShephard.bark()
+#
+#===============================================================
